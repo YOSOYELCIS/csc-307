9888f33ef4f5f0df618a250aa7f733045d726d24
Cis Garcia <yosoyelcis@duck.com>
2026-01-23 02:40:16 -0800

    Updating HTTP queries
    Now I understand the different levels to the http queries, so also completed step 4 in appending queries by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -38,10 +38,15 @@ const findUserByName = (name) => {
     );
 };
 
+const findUserById = (id) => 
+    users["users_list"].find((user) => user["id"] === id);
+
 app.use(express.json());
-// app.get("/", (req, res) => {
-//     res.send("Hello World!");
-// });
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
 app.get("/users", (req, res) => {
     const name= req.query.name;
     if(name != undefined) {
@@ -50,6 +55,16 @@ app.get("/users", (req, res) => {
         res.send(result);
     } else {
         res.send(users);
+    }
+});
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"];
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource Not Found.");
+    } else {
+        res.send(result);
     }
 });
 
